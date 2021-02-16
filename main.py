2b7f2899c1972324918009b0af26b08221cbbdab
denis burnashev <kardenman@gmail.com>
2021-02-16 14:11:35 +0600

    fixes

--- main.py
+++ main.py
@@ -14,36 +14,34 @@ url = 'https://en.wikipedia.org/wiki/'
 class Iteratorq:
 
     def __init__(self, data):
-        self.info = data
+        self.data = data
         self.counter = 0
 
-    def __getitem__(self, i):
-        return self.info[i]
+    def __iter__(self):
+        return self
 
     def __next__(self):
         if self.counter < len(info):
             self.counter += 1
+            for name in self.data:
+                for keys, values in name.items():
+                    if keys == 'name':
+                        for common, count in values.items():
+                            if common == 'common':
+                                countries_list.append(count)
+        for country in countries_list:
+            names = country.replace(' ', '_')
+            urls = (url + names)
+            print(names, '-', urls)
+            with open('links.txt', 'a', encoding='utf-8') as link:
+                link.writelines(f'{names} - {urls}\n')
         else:
             raise StopIteration
 
 
 s_iter1 = Iteratorq(info)
 for name in s_iter1:
-    for keys, values in name.items():
-        if keys == 'name':
-            for common, count in values.items():
-                if common == 'common':
-                    countries_list.append(count)
-print(countries_list)
-
-for country in countries_list:
-    names = country.replace(' ', '_')
-    urls = (url + names)
-    q = (names, urls)
-    url_list.append(q)
-    print(names, '-', urls)
-    with open('links.txt', 'a', encoding='utf-8') as link:
-        link.writelines(f'{names} - {urls}\n')
+    print(name)
 
 with open('links.txt', encoding='utf-8') as country:
     links_info = country.readlines()
@@ -51,15 +49,14 @@ with open('links.txt', encoding='utf-8') as country:
 
 def hash_link(start):
     while start < len(info):
-        yield start
         for each in links_info:
             sig = ''.join(each)
             sig = str.encode(sig, encoding='utf-8')
             sig = hashlib.md5(sig).hexdigest()
-            print(sig)
             with open('md5.txt', 'a', encoding='utf-8') as countries_link:
                 countries_link.writelines(f'{sig}\n')
                 start += 1
+            yield sig
 
 
 for links in hash_link(0):
